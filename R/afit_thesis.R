5d98fa7afecd934761e08976ba7339fe7521dc46
Auburngrads <auburngrads@live.com>
2016-06-02 16:10:35 -0400

    Added rmarkdown/ and R/

---
+++ R/afit_thesis.R
@@ -0,0 +1,24 @@
+#' Air Force Institute of Technology (AFIT) Thesis format.
+#'
+#' Format for creating AFIT Theses.
+#'
+#' @inheritParams rmarkdown::pdf_document
+#' @param ... Arguments to \code{rmarkdown::pdf_document}
+#'
+#' @return R Markdown output format to pass to \code{\link[rmarkdown:render]{render}}
+#'
+#' @examples
+#'
+#' \dontrun{
+#' library(rmarkdown)
+#' draft("MyArticle.Rmd", template = "afit_thesis", package = "AFIT")
+#' }
+#'
+#' @export
+acm_article <- function(...) {
+  pdf_document_format(...,
+                      format = "afit_thesis",
+                      template = "template.tex",
+                      csl = "acm-sig-proceedings.csl")
+}
+
