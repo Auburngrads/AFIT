8e11defbc62be6b331db6e8c92db6841b8d4f66f
Auburngrads <auburngrads@live.com>
2016-06-17 16:54:49 -0400

    Starting the rebuild

--- R/preamble.R
+++ R/preamble.R
@@ -1,8 +1,9 @@
 #' Air Force Institute of Technology Preamble 
 #'
 #' Format for creating the initial pages of AFIT theses, dissertations, and reports.
+#' 
 #' Adapted from the STYLE GUIDE FOR AFIT DISSERTATIONS, THESES AND GRADUATE RESEARCH PAPERS - July 2015
-#' \href{http://www.afit.edu/docs/NewAFITStyleGuideJuly2015(2).pdf}.
+#' \href{http://www.afit.edu/docs/NewAFITStyleGuideJuly2015(2).pdf}{http://www.afit.edu/docs/NewAFITStyleGuideJuly2015(2).pdf}
 #'
 #' @inheritParams rmarkdown::pdf_document
 #' @param ... Arguments to \code{rmarkdown::pdf_document}
