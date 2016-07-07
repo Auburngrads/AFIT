729d1883e2cae932ba8c1233b92512ea2ce7f2fb
Auburngrads <Auburngrads@live.com>
2016-07-06 21:40:48 -0400

    added afit_far

---
+++ R/afit_presentation.R
@@ -0,0 +1,65 @@
+
+#' Create an AFIT Presentation
+#'
+#' Format for converting from R Markdown to a slidy presentation.
+#'
+#' @inheritParams beamer_presentation
+#' @inheritParams pdf_document
+#' @inheritParams html_document
+#'
+#' @param duration Duration (in minutes) of the slide deck. This value is used
+#'   to add a countdown timer to the slide footer.
+#' @param footer Footer text (e.g. organization name and/or copyright)
+#' @param font_adjustment Increase or decrease the default font size
+#'  (e.g. -1 or +1). You can also manually adjust the font size during the
+#'  presentation using the 'S' (smaller) and 'B' (bigger) keys.
+#'
+#' @return R Markdown output format to pass to \code{\link{render}}
+#'
+#' @details
+#'
+#' See the \href{http://rmarkdown.rstudio.com/slidy_presentation_format.html}{online
+#' documentation} for additional details on using the \code{slidy_presentation} format.
+#'
+#' For more information on markdown syntax for presentations see the
+#' \href{http://pandoc.org/README.html}{pandoc online documentation}.
+#'
+#' @examples
+#' \dontrun{
+#'
+#' library(AFIT)
+#'
+#' # simple invocation
+#' render("afit_presentation.Rmd", slidy_presentation())
+#'
+#' # specify an option for incremental rendering
+#' render("afit_presentation.Rmd", slidy_presentation(incremental = TRUE))
+#' }
+#'
+#' @export
+
+afit_presentation <- function(..., 
+                              template = 'default.html',
+                              format = 'afit_presentation',
+                               incremental = FALSE, 
+                               fig_width = 8, 
+                               fig_height = 4.9, 
+                               self_contained = FALSE,
+                               smart = FALSE, 
+                               highlight = "default", 
+                               pandoc_args = '--mathjax=none') {
+
+rmarkdown::slidy_presentation(...,
+                          template = system.file("rmarkdown", "templates", format, 'resources',template,
+                          package = "AFIT"),
+                          incremental = incremental,
+                          fig_width = fig_width,
+                          fig_height = fig_height,
+                          smart = smart,
+                          self_contained = self_contained,
+                          css = 'resources/flat-slidy.css',
+                          highlight = highlight,
+                          includes = rmarkdown::includes(before_body = system.file("rmarkdown", "templates", format, 'resources','jkf-scroll.js',
+                          package = "AFIT")),
+                          pandoc_args = pandoc_args)
+}
