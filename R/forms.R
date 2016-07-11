6a3297ed3e07b38bf91210f6ef01804bbb1baf9b
Auburngrads <Auburngrads@live.com>
2016-07-11 19:31:56 -0400

    Added & Cleaned vignettes

---
+++ R/forms.R
@@ -0,0 +1,35 @@
+#' Open AFIT-Specific forms in a browser  
+#'
+#' @importFrom utils browseURL
+#'
+#' @param form.name The title of the requested form
+#' 
+#' @description This function from to complete an AFIT Thesis, the
+#' @export
+
+
+forms <- function(form.name = NULL) {
+
+  form.name <- gsub('-', '', form.name)
+  form.name <- gsub(' ', '', form.name)
+  form.name <- gsub('.', '', form.name)
+  
+switch(tolower(form.name),
+       'styleguide' =, 
+       'style' =, 
+       'afitstyleguide' =, 
+       'guide' = { browseURL('http://www.afit.edu/docs/NewAFITStyleGuideJuly2015(2).pdf')},
+       
+       'wpafb1420' =, 
+       '1420' =, 
+       'wpafbform1420' =, 
+       'parequest' =, 
+       'parequestform' =,
+       'publicaffairs' =,
+       'publicaffairsform' =,
+       'paform' =,
+       'publicaffairsrequest' =,
+       'publicaffairsrequestform' =,
+       'pa' = { browseURL('http://www.wpafb.af.mil/shared/media/document/AFD-160526-016.pdf') })
+
+}
