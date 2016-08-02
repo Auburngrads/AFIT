804b97033e2b7dd65e0113aae9c647dd8167e087
Auburngrads <Auburngrads@live.com>
2016-08-02 09:39:37 -0400

    clean-up

--- R/dtic.R
+++
@@ -1,6 +0,0 @@
-dtic <- function(access_num = NULL) {
-  
-  url <- paste(c('http://oai.dtic.mil/oai/oai?verb=getRecord&metadataPrefix=html&identifier',
-                  access_num), collapse = '=')
-  
-}
